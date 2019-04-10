852a4fd8b244ea35bd19abbee0b25955c621a38f
Tomas <tmgarcia@miuandes.cl>
2019-04-10 19:37:05 -0400

    event venueeseesessews

--- app/controllers/event_venues_controller.rb
+++ app/controllers/event_venues_controller.rb
@@ -1,10 +1,37 @@
 class EventVenuesController < ApplicationController
+  def new
+  	@eventvenue = EventVenue.new
+  end
+
   def create
+  	@eventvenue = EventVenue.new(params[:user])
+  	respond to do |format|
+  		if @eventvenue.save
+	  		format.html {redirect_to @eventvenue, notice: "eventvenue created"}
+	  		format.json {render json: @eventvenue}
+	  	else
+	  		format.html {render :new}
+	  		format.json {render json: @eventvenue.errors, status: :not_found}
+	  	end
   end
 
   def destroy
+  	@eventvenue = EventVenue.find(params[:id])
+  	respond to do |format|
+		@eventvenue.destroy
+		format.html {redirect_to @eventvenue, notice: "Event venue removed"}
+		format.json {render status: :no_content}
   end
 
   def update
+  	@eventvenue = EventVenue.find(params[:id])
+  	respond to do |format|
+  		@eventvenue.update!(eventvenues_params)
+  		format.html {redirect_to @eventvenue, notice: "Event venue updated"}
+  		format.json {render status: :reset_content}
   end
+  def eventvenues_params
+  	params.require(:eventvenue).permit(:name, :address, :capacity)
+  end
+
 end
