38dddb1a6494116aa2f5b47bdcf448309557b07f
Tomas <tmgarcia@miuandes.cl>
2019-04-10 19:43:51 -0400

    event venueeseesessews

--- app/controllers/event_venues_controller.rb
+++ app/controllers/event_venues_controller.rb
@@ -1,37 +1,37 @@
 class EventVenuesController < ApplicationController
   def new
-  	@eventvenue = EventVenue.new
+  	@event_venue = EventVenue.new
   end
 
   def create
-  	@eventvenue = EventVenue.new(params[:user])
+  	@event_venue = EventVenue.new(params[:user])
   	respond to do |format|
-  		if @eventvenue.save
-	  		format.html {redirect_to @eventvenue, notice: "eventvenue created"}
-	  		format.json {render json: @eventvenue}
+  		if @event_venue.save
+	  		format.html {redirect_to @event_venue, notice: "eventvenue created"}
+	  		format.json {render json: @event_venue}
 	  	else
 	  		format.html {render :new}
-	  		format.json {render json: @eventvenue.errors, status: :not_found}
+	  		format.json {render json: @event_venue.errors, status: :not_found}
 	  	end
   end
 
   def destroy
-  	@eventvenue = EventVenue.find(params[:id])
+  	@event_venue = EventVenue.find(params[:id])
   	respond to do |format|
-		@eventvenue.destroy
-		format.html {redirect_to @eventvenue, notice: "Event venue removed"}
+		@event_venue.destroy
+		format.html {redirect_to @event_venue, notice: "Event venue removed"}
 		format.json {render status: :no_content}
   end
 
   def update
-  	@eventvenue = EventVenue.find(params[:id])
+  	@event_venue = EventVenue.find(params[:id])
   	respond to do |format|
-  		@eventvenue.update!(eventvenues_params)
-  		format.html {redirect_to @eventvenue, notice: "Event venue updated"}
+  		@event_venue.update!(event_venues_params)
+  		format.html {redirect_to @event_venue, notice: "Event venue updated"}
   		format.json {render status: :reset_content}
   end
-  def eventvenues_params
-  	params.require(:eventvenue).permit(:name, :address, :capacity)
+  def event_venues_params
+  	params.require(:event_venue).permit(:name, :address, :capacity)
   end
 
 end
