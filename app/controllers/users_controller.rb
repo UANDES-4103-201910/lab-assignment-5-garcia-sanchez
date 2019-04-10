eafda8c37ecd02d018b695b960db6790cb4b7660
resnakk <masanchez3@miuandes.cl>
2019-04-10 19:20:07 -0400

    users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	respond to do |format|
+  		if @user.save
+	  		format.html {redirect_to @user, notice: "User created"}
+	  		format.json {render json: @user}
+	  	else
+	  		format.html {render :new}
+	  		format.json {render json: @user.errors, status: :not_found}
+	  	end
+  end
+
+  def destroy
+  	@user = User.find(params[:id])
+  	respond to do |format|
+		@user.destroy
+		format.html {redirect_to @user, notice: "User removed"}
+		format.json {render status: :no_content}
+  end
+
+  def update
+  	@user = User.find(params[:id])
+  	respond to do |format|
+  		@user.update!(users_params)
+  		format.html {redirect_to @user, notice: "User updated"}
+  		format.json {render status: :reset_content}
+  end
+  def users_params
+  	params.require(:user).permir(:id, :name)
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'tickets/create'
+  get 'tickets/destroy'
+  get 'tickets/update'
+  get 'event_venues/create'
+  get 'event_venues/destroy'
+  get 'event_venues/update'
+  get 'events/create'
+  get 'events/destroy'
+  get 'events/update'
+  get 'users/create'
+  get 'users/destroy'
+  get 'users/update'
+  get 'users/create'
+  get 'users/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
